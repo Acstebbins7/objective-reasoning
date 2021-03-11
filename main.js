e8a5c9ac392eb9c776c29c8e8d0d2368b4462eef
Adam Stebbins <acstebbins7@gmail.com>
2021-03-10 21:28:44 -0500

    finished it finally

--- main.js
+++ main.js
@@ -1,6 +1,48 @@
 // ┌─────────────────┐
 // │ Your code here! │	
 // └─────────────────┘
+function isAdmin(obj){
+    if (obj.userRole === 'ADMIN'){
+        return true
+    } else return false
+}
+
+// ==============================
+
+function getEmail(obj){
+    return `${obj.firstName.toLowerCase()}.${obj.lastName.toLowerCase()}@codeimmersives.com`
+}
+
+// ==============================
+
+function getPlaylistLength(obj){
+    return obj.songs.length
+}
+
+// ==============================
+
+function getHardestHomework(str){
+    let hard = ""
+    let lowest = 1000
+    for(const score of str){
+        if(score.averageScore < lowest) {
+            lowest = score.averageScore;
+            hard = score.name;
+        }
+    };
+    return hard;
+}
+
+
+// ==============================
+
+function createPhonebook(name, phone){
+    let obj = {};
+    for (let i = 0; i < name.length; i++){
+        obj[name[i]] = phone[i];
+    }
+    return obj;
+}
 
 
 
